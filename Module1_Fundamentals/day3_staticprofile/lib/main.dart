459ff7961fb175b35cba5d481bca6abf665c42f2
aderemi-alo <aloaderemi1@gmail.com>
2025-01-27 14:04:45 +0100

    Built a static profile app in flutter

--- Module1_Fundamentals/day3_staticprofile/lib/main.dart
+++ Module1_Fundamentals/day3_staticprofile/lib/main.dart
@@ -9,11 +9,35 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text('Static Profile'),
         ),
+        body: Center(
+            child: Container(
+          decoration: BoxDecoration(
+            color: Colors.grey.shade200,
+          ),
+          height: 100,
+          width: 300,
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Flutter Developer: Aderemi Alo"),
+                SizedBox(
+                  height: 10,
+                ),
+                Text("Skills: Flutter, Dart"),
+              ],
+            ),
+          ),
+        )),
       ),
     );
   }
